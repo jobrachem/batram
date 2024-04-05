615051cef811ab781677e0cf0e8ca851178d4912
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-04-05 07:50:04 -0500

    Update ttm.py

--- src/batram/ttm.py
+++ src/batram/ttm.py
@@ -10,7 +10,7 @@ from liesel_ptm import (
     optim_flat,
     state_to_samples,
     Stopper,
-    OptimResult
+    OptimResult,
 )
 from dataclasses import dataclass, field
 from scipy import stats
@@ -21,13 +21,22 @@ from typing import Type
 ArrayLike = JaxArray | NumpyArray
 
 
-def setup_ptm(i: int, y: ArrayLike, nparam: int, tau2_cls: Type | None = None, knots: ArrayLike | None = None, **tau2_kwargs):
+def setup_ptm(
+    i: int,
+    y: ArrayLike,
+    nparam: int,
+    tau2_cls: Type | None = None,
+    knots: ArrayLike | None = None,
+    **tau2_kwargs,
+):
     if tau2_cls is None:
         tau2 = VarInverseGamma(1.0, concentration=1.0, scale=0.5, name=f"tau2_{i}")
     else:
-        tau2 = tau2_cls(**tau2_kwargs, name=f"tau2_{i}")
+        tau2 = tau2_cls(**tau2_kwargs.get("tau2_kwargs"), name=f"tau2_{i}")
     if knots is None:
-        model: PTMLocScale = PTMLocScale.from_nparam(y=y, nparam=nparam, normalization_tau2=tau2)
+        model: PTMLocScale = PTMLocScale.from_nparam(
+            y=y, nparam=nparam, normalization_tau2=tau2
+        )
         model, _ = model.optimize_knots(knot_prob_levels=(0.01, 0.99))
     else:
         model: PTMLocScale = PTMLocScale(knots, y, normalization_tau2=tau2)
@@ -39,7 +48,7 @@ def setup_ptm(i: int, y: ArrayLike, nparam: int, tau2_cls: Type | None = None, k
 class PTMFits:
     preds: list[PTMLocScalePredictions]
     models: list[PTMLocScale] = field(default_factory=list)
-    results: list[OptimResult] = field(default_factory=list)
+    results: list[OptimResult | None] = field(default_factory=list)
     fitted_params: list[dict[str, ArrayLike]] = field(default_factory=list)
 
 
@@ -65,10 +74,16 @@ class TransformationTransportMap:
             batch_size=self.batch_size,
         )
         return loss
-    
 
-    def fit_transformation(self, yt: ArrayLike, yt_test: ArrayLike | None = None, nparam: int = 10, max_iter: int = 10_000, patience: int = 100) -> PTMFits:
-        stopper = Stopper(max_iter=max_iter, patience=patience, atol=0.5, rtol=0.01)
+    def fit_transformation(
+        self,
+        yt: ArrayLike,
+        yt_test: ArrayLike | None = None,
+        nparam: int = 10,
+        max_iter: int = 10_000,
+        patience: int = 100,
+    ) -> PTMFits:
+        stopper = Stopper(max_iter=max_iter, patience=patience, atol=0.001, rtol=0.001)
 
         models: list[PTMLocScale] = []
         preds: list[PTMLocScalePredictions] = []
@@ -83,9 +98,13 @@ class TransformationTransportMap:
 
             ptm_test_i = None
             if yt_test is not None:
-                ptm_test_i = setup_ptm(i, yt_test[:, i], nparam=nparam, knots=ptm_i.knots)
+                ptm_test_i = setup_ptm(
+                    i, yt_test[:, i], nparam=nparam, knots=ptm_i.knots
+                )
 
-            results_i = optim_flat(graph_i, params_i, stopper=stopper, model_test=ptm_test_i)
+            results_i = optim_flat(
+                graph_i, params_i, stopper=stopper, model_test=ptm_test_i
+            )
             graph_i.state = results_i.model_state
             fitted_params_i = state_to_samples(all_params_i, graph_i)
             preds_i = PTMLocScalePredictions(fitted_params_i, ptm_i)
@@ -95,7 +114,120 @@ class TransformationTransportMap:
             fitted_params.append(fitted_params_i)
             preds.append(preds_i)
 
-        return PTMFits(models=models, preds=preds, results=results, fitted_params=fitted_params)
+        return PTMFits(
+            models=models, preds=preds, results=results, fitted_params=fitted_params
+        )
+
+    def fit_transformation_switch(
+        self,
+        yt: ArrayLike,
+        yt_test: ArrayLike | None = None,
+        nparam: int = 10,
+        max_iter: int = 10_000,
+        patience: int = 1000,
+    ) -> PTMFits:
+        stopper = Stopper(max_iter=max_iter, patience=patience, atol=0.001, rtol=0.001)
+
+        models: list[PTMLocScale] = []
+        preds: list[PTMLocScalePredictions] = []
+        results: list[OptimResult | None] = []
+        fitted_params: list[dict[str, ArrayLike]] = []
+
+        for i in tqdm(range(yt.shape[1])):
+            ptm_i = setup_ptm(
+                i,
+                yt[:, i],
+                nparam=nparam,
+                tau2_cls=VarInverseGamma,
+                tau2_kwargs={"value": 1.0, "concentration": 3.0, "scale": 0.2},
+            )
+            
+            params_i = ptm_i.all_sampled_parameter_names()
+            all_params_i = ptm_i.all_parameter_names()
+            graph_i = ptm_i.build_graph(optimize_start_values=False)
+
+            ptm_test_i = None
+            if yt_test is not None:
+                ptm_test_i = setup_ptm(
+                    i, yt_test[:, i], nparam=nparam, knots=ptm_i.knots
+                )
+
+            _, p = stats.shapiro(yt[:, i])
+
+            results_i = None
+
+            if p < 0.05:
+                results_i = optim_flat(
+                    graph_i, params_i, stopper=stopper, model_test=ptm_test_i
+                )
+                graph_i.state = results_i.model_state
+
+            fitted_params_i = state_to_samples(all_params_i, graph_i)
+            preds_i = PTMLocScalePredictions(fitted_params_i, ptm_i)
+
+            models.append(ptm_i)
+            results.append(results_i)
+            fitted_params.append(fitted_params_i)
+            preds.append(preds_i)
+
+        return PTMFits(
+            models=models, preds=preds, results=results, fitted_params=fitted_params
+        )
+    
+    def fit_transformation_adaptive(
+        self,
+        yt: ArrayLike,
+        yt_test: ArrayLike | None = None,
+        nparam: int = 10,
+        max_iter: int = 10_000,
+        patience: int = 100,
+    ) -> PTMFits:
+        stopper = Stopper(max_iter=max_iter, patience=patience, atol=0.001, rtol=0.001)
+
+        models: list[PTMLocScale] = []
+        preds: list[PTMLocScalePredictions] = []
+        results: list[OptimResult | None] = []
+        fitted_params: list[dict[str, ArrayLike]] = []
+
+        for i in tqdm(range(yt.shape[1])):
+            _, p = stats.shapiro(yt[:, i])
+            b = 0.003 + (1.0 - p) / 5
+
+            ptm_i = setup_ptm(
+                i,
+                yt[:, i],
+                nparam=nparam,
+                tau2_cls=VarInverseGamma,
+                tau2_kwargs={"value": 1.0, "concentration": 3.0, "scale": b},
+            )
+            
+            params_i = ptm_i.all_sampled_parameter_names()
+            all_params_i = ptm_i.all_parameter_names()
+            graph_i = ptm_i.build_graph(optimize_start_values=False)
+
+            ptm_test_i = None
+            if yt_test is not None:
+                ptm_test_i = setup_ptm(
+                    i, yt_test[:, i], nparam=nparam, knots=ptm_i.knots
+                )
+
+
+            results_i = optim_flat(
+                graph_i, params_i, stopper=stopper, model_test=ptm_test_i
+            )
+            graph_i.state = results_i.model_state
+
+            fitted_params_i = state_to_samples(all_params_i, graph_i)
+            preds_i = PTMLocScalePredictions(fitted_params_i, ptm_i)
+
+            models.append(ptm_i)
+            results.append(results_i)
+            fitted_params.append(fitted_params_i)
+            preds.append(preds_i)
+
+        return PTMFits(
+            models=models, preds=preds, results=results, fitted_params=fitted_params
+        )
 
     def compute_normalization_and_logdet(
         self, fits: PTMFits, yt: ArrayLike | None = None
@@ -111,7 +243,7 @@ class TransformationTransportMap:
             preds: list[PTMLocScalePredictions] = []
             for i, _ in enumerate(fits.preds):
                 pred_i = PTMLocScalePredictions(
-                    fits.fitted_params[i], fits.models[i], y=yt[:,i]
+                    fits.fitted_params[i], fits.models[i], y=yt[:, i]
                 )
                 preds.append(pred_i)
         else:
@@ -124,7 +256,7 @@ class TransformationTransportMap:
         return z.T, logdet.T
 
     def log_score_z(self, z: ArrayLike, logdet: ArrayLike, dim=None):
-        
+
         return (stats.norm.logpdf(z) + logdet).sum(axis=dim)
 
     def fit_iteratively(self): ...
