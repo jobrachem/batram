dc9a6b5cf5df3e4a2f4cd7b4e36aad27a4badaf5
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-06-10 13:30:08 -0500

    Update test_speed.py

--- tests/test_tmspat_jax/test_speed.py
+++ tests/test_tmspat_jax/test_speed.py
@@ -1,32 +1,20 @@
-from collections.abc import Iterator
-
-from timeit import timeit
 import jax.numpy as jnp
 import jax.random as jrd
-import liesel.model as lsl
-import liesel.goose.optim as optim
-import liesel_ptm as ptm
-import pytest
-import tensorflow_probability.substrates.jax.math.psd_kernels as tfk
-import jax
 import veccs.orderings
 
-import batram.tmspat_jax.node_ip as tmip
 import batram.tmspat_jax.node as tm
+import batram.tmspat_jax.node_ip as tmip
 
 key = jrd.PRNGKey(42)
 
 
-
 class TestSpeed:
-
     def test_delta(self):
-
         Nloc = 100
 
-        locs = jrd.uniform(key, shape=(Nloc,2))
+        locs = jrd.uniform(key, shape=(Nloc, 2))
         order = veccs.orderings.maxmin_cpp(locs)
-        locs = locs[order,:]
+        locs = locs[order, :]
 
         K = Nloc // 10
         nobs = 5
@@ -39,24 +27,23 @@ class TestSpeed:
         model = tm.Model(y, knots=knots, locs=locs)
 
         def update_delta_ip():
-            model_ip.delta.latent.value = jrd.normal(key, ((D-2)*K,))
+            model_ip.delta.latent.value = jrd.normal(key, ((D - 2) * K,))
             model_ip.delta.update()
-        
+
         def update_delta():
             latent_delta = model.delta.value_node.kwinputs["latent_delta"].var
-            latent_delta.value = jrd.normal(key, ((D-2)*locs.shape[0],))
+            latent_delta.value = jrd.normal(key, ((D - 2) * locs.shape[0],))
             model.delta.update()
-        
+
         # time_ip = timeit(update_delta_ip, number=100)
         # time_exact = timeit(update_delta, number=100)
-    
-    def test_model(self):
 
+    def test_model(self):
         Nloc = 100
 
-        locs = jrd.uniform(key, shape=(Nloc,2))
+        locs = jrd.uniform(key, shape=(Nloc, 2))
         order = veccs.orderings.maxmin_cpp(locs)
-        locs = locs[order,:]
+        locs = locs[order, :]
 
         K = Nloc // 10
         nobs = 50
@@ -71,16 +58,15 @@ class TestSpeed:
         def update_delta_ip():
             latent_delta = model_ip.delta.latent
             graph = model_ip.build_graph()
-            latent_delta.value = jrd.normal(key, ((D-2)*K,))
+            latent_delta.value = jrd.normal(key, ((D - 2) * K,))
             graph.update()
-            
-        
+
         def update_delta():
             latent_delta = model.delta.value_node.kwinputs["latent_delta"].var
             graph = model.build_graph()
-            latent_delta.value = jrd.normal(key, ((D-2)*locs.shape[0],))
+            latent_delta.value = jrd.normal(key, ((D - 2) * locs.shape[0],))
             graph.update()
-        
+
         # time_ip = timeit(update_delta_ip, number=10)
         # time_exact = timeit(update_delta, number=10)
 
