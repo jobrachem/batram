ba347922e3b3a66c1ab64eb93f89092c7d15cadd
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-07-31 13:47:27 -0500

    Update __init__.py

--- src/batram/tmspat_jax/__init__.py
+++ src/batram/tmspat_jax/__init__.py
@@ -1,12 +1,14 @@
 from liesel_ptm import TransformedVar
 
 from .model import Model, TransformationModel
-from .ppnode import OnionCoefPredictivePointProcessGP, OnionKnots
+from .ppnode import OnionCoefPredictivePointProcessGP, OnionKnots, ModelOnionCoef, ModelVar
 
 __all__ = [
     "Model",
     "TransformationModel",
     "OnionCoefPredictivePointProcessGP",
     "OnionKnots",
+    "ModelOnionCoef",
     "TransformedVar",
+    "ModelVar"
 ]
